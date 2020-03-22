c541eb027e74695d1e3de24d5b3a0b89e867f8d3
SankarCS <sankar.shanmugamc@gmail.com>
2020-03-22 17:02:41 +0100

    Gray Style Checkin
    Gray Style Checkin by Shankar

--- app/style.py
+++ app/style.py
@@ -120,6 +120,65 @@ class White(object):
         }
 
 
+class Gray(object):
+    """Classe para o tema White da calculadora"""
+    def __init__(self):
+        self.master_bg = '#808080'
+        self.frame_bg = '#808080'
+
+        self.INPUT = {
+            'bg': '#f5f6fa',
+            'fg': '#2d3436',
+            'borderwidth': 0,
+            'width': 15,
+            'font': 'Arial 28 bold',
+            'justify': 'right'
+        }
+
+        self.BTN_DEFAULT = {
+            'bg': '#d4d4d4',
+            'fg': '#2d3436',
+            'activebackground': '#635f5f',
+            'activeforeground': '#000000',
+            'borderwidth': 0,
+            'width': 6,
+            'height': 2,
+            'font': 'Arial 14 bold'
+        }
+
+        self.BTN_NUMERICO = {
+            'bg': '#adacac',
+            'fg': '#2d3436',
+            'activebackground': '#635f5f',
+            'activeforeground': '#000000',
+            'borderwidth': 0,
+            'width': 6,
+            'height': 2,
+            'font': 'Arial 14 bold'
+        }
+
+        self.BTN_OPERADOR = {
+            'bg': '#d4d4d4',
+            'fg': '#2d3436',
+            'activebackground': '#0097e6',
+            'activeforeground': '#000000',
+            'borderwidth': 0,
+            'width': 6,
+            'height': 2,
+            'font': 'Arial 14 bold'
+        }
+
+        self.BTN_CLEAR = {
+            'bg': '#d4d4d4',
+            'fg': '#2d3436',
+            'activebackground': '#d63031',
+            'activeforeground': '#000000',
+            'borderwidth': 0,
+            'width': 6,
+            'height': 2,
+            'font': 'Arial 14 bold'
+        }
+
 class DefaultStyleForMacOS(object):
     """Classe criada para ambientes macOS para corrigir bug de estilo."""
     def __init__(self):
